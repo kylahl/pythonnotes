be7b028add8051c7bcd8d0e75f2b517b290c44d8
kylahl <84030977+kylahl@users.noreply.github.com>
2022-09-14 13:30:24 -0400

    Update save_airtable_data_to_path.py

--- save_airtable_data_to_path.py
+++ save_airtable_data_to_path.py
@@ -5,11 +5,13 @@ import os
 from datetime import date
 from dotenv import load_dotenv
 
-load_dotenv()
-DATA_REPO = os.getenv('DATA_REPO')
+#dotenv is a package that is going to look for .env file in my default enviornment and it's going to load the variables in the file
+load_dotenv() 
+DATA_REPO = os.getenv('DATA_REPO') #write what exactly is in the .env file in the (' ') or it won't load and store the variables
 TOKEN = os.getenv('AIRTABLE_TOKEN')
 BASE_ID = os.getenv('AIRTABLE_TEST_BASE_ID')
 
+#connect to airtable using the airtable table name (Table) and table token number (TOKEN)
 test_table = at.Airtable(BASE_ID, 'Table', TOKEN)
 test1 = pd.DataFrame(test_table.get_all())
 test1 = test1.merge(pd.json_normalize(test1['fields']), left_index=True, right_index=True)
